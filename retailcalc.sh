f4108cdeca8c8916e893eafaa8f5159b661ab216
“Serena <serenahathi@hotmail.com>
2018-05-22 17:22:06 +0200

    Refactor tax application logic

--- retailcalc.sh
+++ retailcalc.sh
@@ -9,26 +9,26 @@ ordervalue=$((items*price))
 echo "Order value: ${ordervalue}"
 echo "-"
 
-discount_value () {
+discounter () {
   echo "Your discount value is $1%"
   discountvalue=$(echo "$ordervalue -($ordervalue / 100 * $1)" | bc)
 }
 
 if [ "${ordervalue}" -gt 50000 ]
   then
-    discount_value 15
+    discounter 15
 elif [ "${ordervalue}" -gt 10000 ]
   then
-    discount_value 10
+    discounter 10
 elif [ "${ordervalue}" -gt 7000 ]
   then
-    discount_value 7
+    discounter 7
 elif [ "${ordervalue}" -gt 5000 ]
   then
-    discount_value 5
+    discounter 5
 elif [ "${ordervalue}" -gt 1000 ]
   then
-    discount_value 3
+    discounter 3
 else
   echo "No discount applicable!"
     discountvalue=$(echo "$ordervalue *1" | bc)
@@ -36,27 +36,26 @@ fi
 echo "Order value after discounts: ${discountvalue}"
 echo "-"
 
+tax () {
+  echo "$state tax applied: $1%"
+  taxedvalue=$(echo "scale=2; $discountvalue +($discountvalue / 100 * $1)" | bc)
+}
 
- case $state in
-   UT)
-    echo "Your tax rate value is 6.85%"
-    taxedvalue=$(echo "$discountvalue *1.0685" | bc)
+case $state in
+  UT)
+    tax 6.85
     ;;
-   NV)
-    echo "Your tax rate value is 8.00%"
-    taxedvalue=$(echo "$discountvalue *1.08" | bc)
+  NV)
+    tax 8
     ;;
-   TX)
-    echo "Your tax rate value is 6.25%"
-    taxedvalue=$(echo "$discountvalue *1.0625" | bc)
+  TX)
+    tax 6.25
     ;;
-   AL)
-    echo "Your tax rate value is 4.00%"
-    taxedvalue=$(echo "$discountvalue *1.04" | bc)
+  AL)
+    tax 4
     ;;
-   CA)
-    echo "Your tax rate value is 8.25%"
-    taxedvalue=$(echo "$discountvalue *1.0825" | bc)
+  CA)
+    tax 8.25
     ;;
    *)
     echo "State not known - no tax applied"
